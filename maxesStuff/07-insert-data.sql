db94930b0d6d2d63c0792d394a00c7446f00a757
XCode0033 <XCode0033@gmail.com>
2026-03-12 18:20:21 -0400

    studied

---
+++ maxesStuff/07-insert-data.sql
@@ -0,0 +1,28 @@
+INSERT INTO employers (
+    company_name,
+    company_address,
+    yearly_revenue,
+    is_hiring
+  )
+VALUES ('Learning INC', 'Education Street', 99.87, TRUE);
+INSERT INTO employers (
+    company_name,
+    company_address,
+    yearly_revenue,
+    is_hiring
+  )
+VALUES ('MATH INC', 'Number Street', 3.26, FALSE);
+INSERT INTO employers (
+    company_name,
+    company_address,
+    yearly_revenue,
+    is_hiring
+  )
+VALUES ('SCHOOL INC', 'TRI-C Street', 2.87, TRUE);
+INSERT INTO conversations (user_name, employer_name, message, date_sent)
+VALUES (
+    'Xavier Stone',
+    'Learning Inc',
+    'Hi I like coding',
+    '2021-10-15 16:32:10'
+  )
---
+++ sharp_schedule.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE sharp_schedule;
